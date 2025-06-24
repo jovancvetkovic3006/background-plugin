08f5f70da1e504b8d91fa529da20aa9526824daf
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2025-06-24 16:14:15 +0200

    1

--- android/src/main/java/ionic/jejkalinkui/plugins/background/Background.java
+++ android/src/main/java/ionic/jejkalinkui/plugins/background/Background.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class Background {
 
     public String echo(String value) {
-        Log.i("Echo", value);
+        Log.i("BackgroundService", "Echo value: " + value);
         return value;
     }
 }
--- android/src/main/java/ionic/jejkalinkui/plugins/background/BackgroundPlugin.java
+++ android/src/main/java/ionic/jejkalinkui/plugins/background/BackgroundPlugin.java
@@ -5,6 +5,7 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import android.util.Log;
 
 @CapacitorPlugin(name = "Background")
 public class BackgroundPlugin extends Plugin {
@@ -14,9 +15,11 @@ public class BackgroundPlugin extends Plugin {
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
-
+        Log.i("BackgroundPlugin", "Echo called with value: " + value);
         JSObject ret = new JSObject();
         ret.put("value", implementation.echo(value));
+
+        notifyListeners("onDataFetched", ret);
         call.resolve(ret);
     }
 }
